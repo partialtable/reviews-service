74c798bc1bbfa6429b984282475d6ed2c2ceb1d4
chrisalopez1337 <chrisalopez@outlook.com>
2020-10-13 23:00:04 -0700

    Added CRUD API Endpoints to README

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ module.exports = {
   host: 'mysql',
   user: 'root',
   // if you are using the pairing stations, the user is `student`
-  password: 'mysql',
+  password: 'password',
   // if you are using the paring stations, the password is `student`
   // if you do not have a password, delete this key/value pair from
   // the object
