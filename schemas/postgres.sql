a247ea8fb19ae203f06579414b580cadf53955a1
chrisalopez1337 <chrisalopez@outlook.com>
2020-10-15 11:56:54 -0700

    Added seeding generator script for User Table as well as copy command. Both in relation to Postgres DB. Tested and insert correctly, using fake URL for now but can refactor easily

--- schemas/postgres.sql
+++ schemas/postgres.sql
@@ -17,13 +17,10 @@ CREATE TABLE reviews_service.restuarant (
   loved_for_array TEXT [],
   filters_array TEXT []
 );
-
-CREATE TABLE reviews_service.test (
-  restuarant_id SERIAL PRIMARY KEY,
-  restuarant_name TEXT,
-  number_of_reviews INTEGER,
-  food_overall_rating DECIMAL
-);
+-- Copy Command for CSV to rest
+COPY reviews_service.restuarant (restuarant_name, number_of_reviews, food_overall_rating, service_overall_rating, ambiance_overall_rating, overall_rating, noise_level, one_star_percent, two_star_percent, three_star_percent, four_star_percent, five_star_percent, loved_for_array, filters_array) FROM '/Users/pepe/pepe/HR/SDC/reviews-service/RestaurantTable.csv' DELIMITERS ',' CSV header;
+-- Copy command for CSV to users
+COPY reviews_service.user (first_name, last_name, location, avatar_url) FROM '/Users/pepe/pepe/HR/SDC/reviews-service/UserTable.csv' DELIMITERS ',' CSV header;
 
 CREATE TABLE reviews_service.user (
   user_id SERIAL PRIMARY KEY,
